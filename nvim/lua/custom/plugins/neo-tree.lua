b9d2b379a1fe43deb5abb06413ca669b7d1f303e
Oliver Zernikow <zernikowoliver@gmail.com>
2026-01-21 11:52:14 +0100

    neotree nt hotkey

--- nvim/lua/custom/plugins/neo-tree.lua
+++ nvim/lua/custom/plugins/neo-tree.lua
@@ -11,7 +11,7 @@ return {
   },
   lazy = false,
   keys = {
-    { '##', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { 'nt', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
     filesystem = {
