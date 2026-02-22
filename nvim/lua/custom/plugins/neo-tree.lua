fc9629dbc3a6e81bab2adc1aa929a616b4ef5f88
Oliver Zernikow <zernikowoliver@gmail.com>
2026-02-22 19:08:17 +0100

    nT command

--- nvim/lua/custom/plugins/neo-tree.lua
+++ nvim/lua/custom/plugins/neo-tree.lua
@@ -11,13 +11,23 @@ return {
   },
   lazy = false,
   keys = {
-    { 'nt', ':Neotree toggle<CR>', desc = 'NeoTree reveal', silent = true },
+    { 'nt', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { 'nT', ':Neotree toggle<CR>', desc = 'NeoTree toggle', silent = true },
   },
   opts = {
     filesystem = {
+      filtered_items = {
+        hide_dotfiles = false,
+        hide_gitignored = true,
+      },
       window = {
         mappings = {
           ['\\'] = 'close_window',
+          -- space being the leader key waits for a few ms for another key stroke before opening a directory in the Neotree window. Thus, it seems like a delay. Therefore we ignore the wait in Neotree window. (alternative is to use `w` instead)
+          ['<space>'] = {
+            'toggle_node',
+            nowait = true, -- 👈 THIS removes the delay
+          },
         },
       },
     },
